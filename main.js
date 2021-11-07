cd4c9f11b9ae7c7260d226f0d6642a986b3e5620
Hai Trieu Ngoc <trieungochai.dev@gmail.com>
2021-11-07 13:31:20 +0700

    Connect to NodeExpressProjects (mongodb)

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
+import mongoose from "mongoose";
 
 import posts from "./routes/posts.js";
 
 const app = express();
+const PORT = process.env.port || 5000;
+const URI =
+  "mongodb+srv://admin:911225@nodeexpressprojects.budye.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 app.use(bodyParser.json({ limit: "30mb" }));
 app.use(bodyParser.urlencoded({ extended: true, limit: "30mb" }));
@@ -12,7 +16,14 @@ app.use(cors());
 
 app.use("/posts", posts);
 
-const PORT = process.env.port || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+mongoose
+  .connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connected to DB");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
